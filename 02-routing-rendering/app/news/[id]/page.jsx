545b1b814ed5fad8b18604b55d5e791a305b4c2f
raccoonroman <kromanjs@gmail.com>
2026-02-19 23:10:55 +0200

    install second project and add news pages

---
+++ 02-routing-rendering/app/news/[id]/page.jsx
@@ -0,0 +1,8 @@
+export default function NewsDetailPage({ params }) {
+  return (
+    <div>
+      <h1>News Item: {params.id}</h1>
+      <p>This is the news item page for {params.id}.</p>
+    </div>
+  );
+}
---
+++ 02-routing-rendering/app/news/page.jsx
@@ -0,0 +1,8 @@
+export default function NewsPage() {
+  return (
+    <div>
+      <h1>News</h1>
+      <p>This is the news page.</p>
+    </div>
+  );
+}
