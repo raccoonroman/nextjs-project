9a77a8a98a2b54bd3b196b912db255064559105d
raccoonroman <kromanjs@gmail.com>
2026-03-01 23:34:04 +0200

    163. Showing a "Loading" Fallback

--- 02-routing-rendering/app/api/route.js
+++ 02-routing-rendering/app/api/route.js
@@ -1,4 +1,3 @@
 export const GET = (request) => {
-  console.log(request);
   return new Response('Hello');
 };
--- 02-routing-rendering/middleware.js
+++ 02-routing-rendering/middleware.js
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  console.log('Middleware is running');
-  console.log(request);
   return NextResponse.next();
 }
 
