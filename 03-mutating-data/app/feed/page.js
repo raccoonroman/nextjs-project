569e94513e893052a3c9169450de60c5d70b8288
raccoonroman <kromanjs@gmail.com>
2026-03-05 17:53:30 +0200

    205. Add metadata

--- 03-mutating-data/app/feed/page.js
+++ 03-mutating-data/app/feed/page.js
@@ -1,6 +1,14 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+export const generateMetadata = async (config) => {
+  const posts = await getPosts();
+  return {
+    title: `Feed - ${posts.length} posts`,
+    description: `See all the posts from your friends and family. There are currently ${posts.length} posts in your feed.`,
+  };
+};
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
--- 03-mutating-data/app/page.js
+++ 03-mutating-data/app/page.js
@@ -3,6 +3,11 @@ import { Suspense } from 'react';
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+export const metadata = {
+  title: 'Latest Posts',
+  description: 'See the latest posts from your friends and family.',
+};
+
 async function LatestPosts() {
   const latestPosts = await getPosts(2);
   return <Posts posts={latestPosts} />;
@@ -14,9 +19,9 @@ export default async function Home() {
       <h1>Welcome back!</h1>
       <p>Here's what you might've missed.</p>
       <section id="latest-posts">
-      <Suspense fallback={<p>Loading recent posts...</p>}>
-        <LatestPosts />
-      </Suspense>
+        <Suspense fallback={<p>Loading recent posts...</p>}>
+          <LatestPosts />
+        </Suspense>
       </section>
     </>
   );
