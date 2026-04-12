44e5707a7e25c998e4a723d3fb70aa96ab2acd72
raccoonroman <kromanjs@gmail.com>
2026-04-12 23:51:19 +0300

    269. Running Server-side Code & Using the Filesystem

--- 07-page-prerendering-data-fetching/pages/index.js
+++ 07-page-prerendering-data-fetching/pages/index.js
@@ -1,11 +1,24 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 function HomePage(props) {
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const fileContents = await fs.readFile(filePath, 'utf-8');
+  const products = JSON.parse(fileContents).products;
+  return {
+    props: { products },
+  };
 }
 
 export default HomePage;
